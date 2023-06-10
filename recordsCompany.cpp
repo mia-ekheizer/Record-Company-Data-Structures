bae0f3d5719e2beb1469e9e7ef08dc97377bf357
Mia Ekheizer <miaekh12@gmail.com>
2023-06-10 14:03:07 +0300

    created recordsCompany.cpp

---
+++ recordsCompany.cpp
@@ -0,0 +1,49 @@
+#include "recordsCompany.h"
+
+RecordsCompany::RecordsCompany() {
+
+}
+
+RecordsCompany::~RecordsCompany() {
+
+}
+
+StatusType RecordsCompany::newMonth(int *records_stocks, int number_of_records) {
+
+}
+
+StatusType RecordsCompany::addCostumer(int c_id, int phone) {
+
+}
+
+Output_t<int> RecordsCompany::getPhone(int c_id) {
+
+}
+
+StatusType RecordsCompany::makeMember(int c_id) {
+
+}
+
+Output_t<bool> RecordsCompany::isMember(int c_id) {
+
+}
+
+StatusType RecordsCompany::buyRecord(int c_id, int r_id) {
+
+}
+
+StatusType RecordsCompany::addPrize(int c_id1, int c_id2, double  amount) {
+
+}
+
+Output_t<double> RecordsCompany::getExpenses(int c_id) {
+
+}
+
+StatusType RecordsCompany::putOnTop(int r_id1, int r_id2) {
+
+}
+
+StatusType RecordsCompany::getPlace(int r_id, int *column, int *hight) {
+
+}
